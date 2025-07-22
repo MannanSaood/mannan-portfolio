0d8de96ac010a9cbb04c4714e12b34319e3873df
mannansaood <syed.mannan@outlook.com>
2025-07-23 04:21:36 +0530

    Changes for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { AnimationStyles, EmberBackground, MistBackground } from './components/Animations';
 import Header from './components/Header';
 import Hero from './components/Hero';
